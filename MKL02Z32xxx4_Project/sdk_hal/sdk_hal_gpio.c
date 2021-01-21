4de652554e8b5a9ba3aa331e55b6e93b21171f54
LAPTOP-1KAV57JR\hp <alvaronarvaez97@gmail.com>
2021-01-20 21:28:37 -0500

    actualizacion de libreria para configuracion de GPIO de MCUXpresso

--- MKL02Z32xxx4_Project/sdk_hal/sdk_hal_gpio.c
+++ MKL02Z32xxx4_Project/sdk_hal/sdk_hal_gpio.c
@@ -1,8 +1,8 @@
 /*! @file : sdk_hal_gpio.c
- * @author  Alvaro Narveaz
+ * @author  Ernesto Andres Rincon Cruz
  * @version 1.0.0
- * @date    19/01/2021
- * @brief   Driver para 
+ * @date    10/01/2021
+ * @brief   Driver for GPIO
  * @details
  *
 */
@@ -34,11 +34,150 @@
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
-status_t gpioPutHigh(uint8_t ping_to_change){
 
-}
 
 /*******************************************************************************
  * Public Source Code
  ******************************************************************************/
- 
+/*--------------------------------------------*/
+status_t gpioPutHigh(uint16_t pin_to_change) {
+
+	uint16_t gpio_port_name;
+	uint32_t gpio_pin_number;
+
+	//Take 8MSB bits of pin_name for GPIOx PORT NAME
+	gpio_port_name = pin_to_change & 0xFF00;
+	gpio_port_name >>= 8;
+
+	//Take 8LSB bits of pin_name for GPIOx PIN NUMBER
+	gpio_pin_number = pin_to_change & 0x00FF;
+
+	//only is available numbers for range (0-31)
+	if (gpio_pin_number > 31)
+		return (kStatus_Fail);
+
+	//pin mask is calculated
+	gpio_pin_number = 1 << (gpio_pin_number);
+
+	//Call NXP_SDK_STACK to perform pin change
+	switch (gpio_port_name) {
+	case KGPIOA:
+		GPIO_PortSet(GPIOA, gpio_pin_number);
+		break;
+
+	case KGPIOB:
+		GPIO_PortSet(GPIOB, gpio_pin_number);
+		break;
+
+	default:
+		return (kStatus_Fail);
+		break;
+	}
+
+	return (kStatus_Success);
+}
+ /*--------------------------------------------*/
+status_t gpioPutLow(uint16_t pin_to_change) {
+	uint16_t gpio_port_name;
+	uint32_t gpio_pin_number;
+
+	//Take 8MSB bits of pin_name for GPIOx PORT NAME
+	gpio_port_name = pin_to_change & 0xFF00;
+	gpio_port_name >>= 8;
+
+	//Take 8LSB bits of pin_name for GPIOx PIN NUMBER
+	gpio_pin_number = pin_to_change & 0x00FF;
+
+	//only is available numbers for range (0-31)
+	if (gpio_pin_number > 31)
+		return (kStatus_Fail);
+
+	//pin mask is calculated
+	gpio_pin_number = 1 << (gpio_pin_number);
+
+	//Call NXP_SDK_STACK to perform pin change
+	switch (gpio_port_name) {
+	case KGPIOA:
+		GPIO_PortClear(GPIOA, gpio_pin_number);
+		break;
+
+	case KGPIOB:
+		GPIO_PortClear(GPIOB, gpio_pin_number);
+		break;
+
+	default:
+		return (kStatus_Fail);
+		break;
+	}
+
+	return (kStatus_Success);
+}
+ /*--------------------------------------------*/
+status_t gpioPutValue(uint16_t pin_to_change, uint8_t new_value) {
+	uint16_t gpio_port_name;
+	uint32_t gpio_pin_number;
+
+	//Take 8MSB bits of pin_name for GPIOx PORT NAME
+	gpio_port_name = pin_to_change & 0xFF00;
+	gpio_port_name >>= 8;
+
+	//Take 8LSB bits of pin_name for GPIOx PIN NUMBER
+	gpio_pin_number = pin_to_change & 0x00FF;
+
+	//only is available numbers for range (0-31)
+	if (gpio_pin_number > 31)
+		return (kStatus_Fail);
+
+	//Call NXP_SDK_STACK to perform pin change
+	switch (gpio_port_name) {
+	case KGPIOA:
+		GPIO_PinWrite(GPIOA, gpio_pin_number,new_value);
+		break;
+
+	case KGPIOB:
+		GPIO_PinWrite(GPIOB, gpio_pin_number,new_value);
+		break;
+
+	default:
+		return (kStatus_Fail);
+		break;
+	}
+
+	return (kStatus_Success);
+}
+ /*--------------------------------------------*/
+status_t gpioPutToggle(uint16_t pin_to_change) {
+	uint16_t gpio_port_name;
+	uint32_t gpio_pin_number;
+
+	//Take 8MSB bits of pin_name for GPIOx PORT NAME
+	gpio_port_name = pin_to_change & 0xFF00;
+	gpio_port_name >>= 8;
+
+	//Take 8LSB bits of pin_name for GPIOx PIN NUMBER
+	gpio_pin_number = pin_to_change & 0x00FF;
+
+	//only is available numbers for range (0-31)
+	if (gpio_pin_number > 31)
+		return (kStatus_Fail);
+
+	//pin mask is calculated
+	gpio_pin_number = 1 << (gpio_pin_number);
+
+	//Call NXP_SDK_STACK to perform pin change
+	switch (gpio_port_name) {
+	case KGPIOA:
+		GPIO_PortToggle(GPIOA, gpio_pin_number);
+		break;
+
+	case KGPIOB:
+		GPIO_PortToggle(GPIOB, gpio_pin_number);
+		break;
+
+	default:
+		return (kStatus_Fail);
+		break;
+	}
+
+	return (kStatus_Success);
+}
